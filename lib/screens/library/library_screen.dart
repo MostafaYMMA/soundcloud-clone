547fa3e60b7e82286fc28b4ce878e57b1d418638
HanaAhmed30 <169292385+HanaAhmed30@users.noreply.github.com>
2026-04-29 01:46:43 +0000

    style: auto-format dart code [skip ci]

--- lib/screens/library/library_screen.dart
+++ lib/screens/library/library_screen.dart
@@ -87,7 +87,6 @@ class LibraryScreen extends StatelessWidget {
                 LibraryTile(
                   title: 'Following',
                   onTap: () => onNavigate(FollowingScreen(onBack: onBack)),
-                 
                 ),
                 const SizedBox(height: AppDimensions.spaceSmall),
                 LibraryTile(
