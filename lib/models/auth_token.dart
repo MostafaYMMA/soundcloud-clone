4f14128ee1de9f4e90e9e456d98ca747fb7e1ce0
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-04-30 13:39:04 +0300

    Update auth_token.dart

--- lib/models/auth_token.dart
+++ lib/models/auth_token.dart
@@ -2,7 +2,10 @@ class AuthTokens {
   final String accessToken;
   final String refreshToken;
 
-  const AuthTokens({required this.accessToken, required this.refreshToken});
+  const AuthTokens({
+    required this.accessToken,
+    required this.refreshToken,
+  });
 
   factory AuthTokens.fromJson(Map<String, dynamic> json) {
     final data = json['data'] as Map<String, dynamic>? ?? json;
@@ -11,5 +14,16 @@ class AuthTokens {
       accessToken: data['access_token']?.toString() ?? '',
       refreshToken: data['refresh_token']?.toString() ?? '',
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'access_token': accessToken,
+      'refresh_token': refreshToken,
+    };
+  }
+
+  bool get isValid {
+    return accessToken.isNotEmpty && refreshToken.isNotEmpty;
   }
 }
