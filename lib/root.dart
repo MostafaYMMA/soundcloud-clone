25363d8ccae2518ac6d498fdc2b5add9fd150faa
HanaAhmed30 <169292385+HanaAhmed30@users.noreply.github.com>
2026-05-02 23:47:00 +0000

    style: auto-format dart code [skip ci]

--- lib/root.dart
+++ lib/root.dart
@@ -302,7 +302,11 @@ class _RootScreenState extends ConsumerState<RootScreen> {
   List<Widget> _buildScreens() => [
     HomeScreen(onTrackTap: _handlePlay, onQueuePlay: _setQueueAndPlay),
     FeedScreen(onTrackTap: _handlePlay),
-    SearchScreen(onTrackTap: _handlePlay, onNavigate: _pushSubScreen, onQueuePlay: _setQueueAndPlay,),
+    SearchScreen(
+      onTrackTap: _handlePlay,
+      onNavigate: _pushSubScreen,
+      onQueuePlay: _setQueueAndPlay,
+    ),
     LibraryScreen(
       onNavigate: _pushSubScreen,
       onBack: _popSubScreen,
